c3aca75e310c6b19156ea78fec7828881b1f53e7
SanthiyaK <santhiyasantthosh@gmail.com>
2025-12-12 15:17:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const ProductRoute = require("./routes/productRoute")
+const dotenv = require("dotenv");
+const connectdb = require("./config/db");
+dotenv.config();
+connectdb();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", ProductRoute);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
